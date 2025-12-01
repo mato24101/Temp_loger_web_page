03a1ebfaa610bdd14ca84d1eaf929d29e37c268f
mato24101 <mato24101@gmail.com>
2025-12-01 14:50:20 +0100

    api key change

--- netlify/functions/teplota-zapis.js
+++ netlify/functions/teplota-zapis.js
@@ -17,7 +17,7 @@ exports.handler = async (event) => {
     // -------------------------------------------------------------
     // KONTROLA AUTENTIFIKÁCIE: Netlify funkcia overuje ESP32 kľúč
     // -------------------------------------------------------------
-    const actualApiKey = event.headers['x-api-key'];
+    const actualApiKey = event.headers['ESP32_API_KEY'];
 
     if (!actualApiKey || actualApiKey !== expectedApiKey) {
         // Ak kľúč chýba alebo je nesprávny, vrátime 401
